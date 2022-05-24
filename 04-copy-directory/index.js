2e95ad8316e5f091f3e41f79091a9c3aaa3ee7e4
Slava Slivka <aphelion.unet@gmail.com>
2022-05-24 05:09:04 +0300

    feat: add 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 
+const dir = path.resolve(__dirname, 'files');
+const dirToCopy = path.resolve(__dirname, 'files-copy');
+
+fs.rm(path.resolve(__dirname, 'files-copy'), { recursive: true, force: true }, () => {
+  fs.promises.mkdir(dirToCopy, {recursive: true})
+  .then(() => {
+    fs.readdir(dir, (err, files) => {
+      files.forEach((file) => {
+        fs.copyFile(path.resolve(dir, file), path.resolve(dirToCopy, file), () => {});
+      });
+    });
+  });
+});
