39a6c089e1feaad3fdd98d24ce9cd986c373ba17
Slava Slivka <aphelion.unet@gmail.com>
2022-05-24 05:10:56 +0300

    feat: add 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const outputFile = fs.createWriteStream(path.resolve(__dirname, 'project-dist', 'bundle.css'));
+
+fs.promises.readdir(path.resolve(__dirname, 'styles'), {withFileTypes: true})
+	.then((files) => {
+		files.forEach((file) => {
+			if (file.isFile() && file.name.split('.')[1] === 'css') {
+				const stream = fs.createReadStream(path.resolve(__dirname, 'styles', file.name));
+				let data = '';
+
+				stream.on('data', (chunk) => outputFile.write(data += chunk + '\n'));
+			}
+		});
+	})
